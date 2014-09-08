6ed67abdf0b57ef68875eb1c6c29b21fdc7a6e58
charliedowler <charliedowler15@gmail.com>
2014-09-08 22:53:15 +0100

    fixed command

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 var Q = require('q');
 module.exports = function (callback) {
   var deferred = Q.defer();
-  exec('couchdb', function (err, stdout, stderr) {
+  exec('couchdb -s', function (err, stdout, stderr) {
     var result = {
       type: 'couchdb',
       running: (err || !/is running/i.test(stdout)) ? false : true
